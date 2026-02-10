609317dc651737e055e57b955907f35bccf6a379
domzmojar <domzmojar01@gmail.com>
2026-02-11 03:21:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     currency: "₱",
     // 1. UPDATE THIS TO YOUR FACEBOOK PAGE LINK
-    messengerUrl: "https://m.me/domzmojar", 
+    messengerUrl: "https://m.me/khy.canillo", 
     // 2. PASTE YOUR PUBLISHED GOOGLE SHEET CSV LINK HERE
     sheetUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRBquyZXkcMOzDv_14qyXq7sQvxqQ6k1l6tWZsiqspZ_mgl88Lqx08h3wUVYu9W9-MIP-ja5f-Yvtsj/pub?gid=1109857950&single=true&output=csv" 
 };
@@ -165,4 +165,5 @@ function showToast(m) {
 }
 
 loadProducts();
+
 
